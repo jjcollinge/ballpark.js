a59a929eaa8e0119fdac01e217b113e0ee7de543
Jon Collinge <joni.collinge@student.shu.ac.uk>
2015-02-07 23:56:16 +0000

    Moved utility functions into the node class

--- node.js
+++ node.js
@@ -2,6 +2,9 @@
  * Node represents a geographical point
  */
  
+// Dependencies
+var request = require("request");
+ 
 function Node(lon, lat) {
     this.lon = lon;
     this.lat = lat;
@@ -13,6 +16,42 @@ Node.prototype.addAltitude = function(alt) {
 
 Node.prototype.addAccuracy = function(acc) {
     this.acc = acc;
+}
+
+Node.prototype.attachIP = function(ip, callback) {
+    var url = 'http://freegeoip.net/json/' + ip;
+    
+    request(url, function(error, response, body) {
+        if(error) return console.error(error);
+        this.lon = body.longitude;
+        this.lat = body.latitude;
+        this.country = body.country_name;
+        this.city = body.city;
+        this.postcode = body.zip_code;
+        this.timezone = body.time_zone;
+        this.ip = body.ip;
+        callback();
+    });
+}
+
+Node.prototype.distanceInKMFrom = function(otherNode) {
+    var R = 6371; // Radius of the earth in km
+    var latDiff = convertDegreesToRadians(otherNode.lat - this.lat);  
+    var lonDif = convertDegreesToRadians(otherNode.lon - this.lon); 
+    
+    var a = 
+    Math.sin(latDiff/2) * Math.sin(latDiff/2) +
+    Math.cos(convertDegreesToRadians(this.lat)) *
+    Math.cos(convertDegreesToRadians(otherNode.lat)) * 
+    Math.sin(lonDif/2) * Math.sin(lonDif/2); 
+    
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function convertDegreesToRadians(deg) {
+    return deg * (Math.PI/180);
 }
 
 module.exports = Node;
--- test/spec/utility-spec.js
+++ test/spec/utility-spec.js
@@ -4,43 +4,54 @@
 
 // Dependencies
 var app = require("../../app");
-var util = require("../../utility");
 var Node = require("../../node");
 
 describe("calculate distance", function() {
     it("should calculate a distance of 157km", function() {
         var srcNode = new Node(0.0000, 0.0000);
         var desNode = new Node(1.0000, 1.0000);
-        var distance = Math.floor(util.calculateDistanceInKm(srcNode, desNode));
+        var distance = Math.floor(srcNode.distanceInKMFrom(desNode));
         expect((distance)).toBe(157);
     });
     it("should calculate a distance of 0km", function() {
         var srcNode = new Node(0.0000, 0.0000);
         var desNode = new Node(0.0000, 0.0000);
-        var distance = Math.floor(util.calculateDistanceInKm(srcNode, desNode));
+        var distance = Math.floor(srcNode.distanceInKMFrom(desNode));
         expect((distance)).toBe(0);
     });
 });
 
 describe("calculate current geo location", function() {
     it("should return my location", function() {
-            var location = util.getCurrentLocation("31.205.3.227", function() {
-            var lat = Math.floor(location.latitude);
-            var lon = Math.floor(location.longitude);
-            expect(lat).toBe(53);
-            expect(lon).toBe(-1);
-       });
+        var testNode = new Node(0, 0);
+        testNode.attachIP("31.205.3.227", function() {
+            expect(testNode.lat).toBe(Math.floor(53));
+            expect(testNode.lon).toBe(Math.floor(-1));
+        });
     });
     it("should return my city", function() {
-            var location = util.getCurrentLocation("31.205.3.227", function() {
-            var city = location.city;
-            expect(city).toBe("Sheffield");
-       }) 
+        var testNode = new Node(0, 0);
+        testNode.attachIP("31.205.3.227", function() {
+            expect(testNode.city).toBe("Sheffield");
+        });
     });
     it("should return my country", function() {
-            var location = util.getCurrentLocation("31.205.3.227", function() {
-            var country = location.country_name;
-            expect(city).toBe("United Kingdom");
-       }) 
+        var testNode = new Node(0, 0);
+        testNode.attachIP("31.205.3.227", function() {
+            expect(testNode.country).toBe("United Kingdom");
+        });
+    });
+    it("should return my timezone", function() {
+        var testNode = new Node(0, 0);
+        testNode.attachIP("31.205.3.227", function() {
+            expect(testNode.timezone).toBe("Europe/London");
+        });
+    });
+    it("should return my ip", function() {
+        var testNode = new Node(0, 0);
+        testNode.attachIP("31.205.3.227", function() {
+            expect(testNode.ip).toBe("31.205.3.227");            
+        });
+
     });
 })
